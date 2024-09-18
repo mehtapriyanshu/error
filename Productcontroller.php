18dc6efef279d083cfcd26cbf6d46555d3446bc5
Priyanshu Mehta <125463390+mehtapriyanshu@users.noreply.github.com>
2024-09-18 16:48:54 +0530

    Add files via upload

---
+++ Productcontroller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\controllers;
+
+class Productcontroller
+{
+    public function index()
+    {
+        echo "index page";
+    }
+
+    public function create()
+    {
+        echo "create product";
+    }
+
+    public function update()
+    {
+        echo "update product";
+    }
+
+    public function delete()
+    {
+        echo "delete product";
+    }
+}
